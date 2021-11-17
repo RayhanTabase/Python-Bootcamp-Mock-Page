e952fd7b4af39d585f4a67e9fa959395d7eaf68b
Rayhan <nofussdufus@gmail.com>
2021-11-17 15:24:10 +0000

    Add dynamic featured speakers js content, Add screen size responsiveness

--- static/js/displayInstructors.js
+++ static/js/displayInstructors.js
@@ -1,3 +1,24 @@
 import instructors from "./instructors.js";
 
-console.log(instructors)
+const instructorsSection = document.querySelector('#featured-instructors');
+export default function displayInstructors(){
+  const listContainer = document.createElement('ul');
+  listContainer.className = 'instructors-list';
+
+  instructors.forEach((instructor) => {
+    const instructorCard = document.createElement('li');
+    instructorCard.className = 'instructor-card';
+    instructorCard.innerHTML = `
+      <div class="text">
+        <h3 class="name">${instructor.name}</h3>
+        <p class="job">${instructor.job}</p>
+        <p class="details">${instructor.details}</p>
+      </div>
+      <div class="image">
+        <img src="${instructor.image}" alt="${instructor.name}">
+      </div>
+    `
+    listContainer.appendChild(instructorCard);
+  })
+  instructorsSection.appendChild(listContainer);
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,9 @@
 import addHamburgerMenu from './hamburger.js'
+import displayInstructors from './displayInstructors.js';
+
 function main() {
   addHamburgerMenu()
+  displayInstructors()
 }
 
 window.onload = main;
